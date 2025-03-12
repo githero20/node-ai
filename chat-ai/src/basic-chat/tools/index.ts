ef398f079b4ed03d4295279b2500fb88d564447b
Omogbai Atakpu <atakpuomogbai@gmail.com>
2025-03-12 16:44:52 +0100

    ft: add more tools

---
+++ chat-ai/src/basic-chat/tools/index.ts
@@ -0,0 +1,105 @@
+import OpenAI from "openai";
+
+const openAI = new OpenAI();
+
+function getTimeOfDay() {
+  return "5:45pm";
+}
+
+function getOrderStatus(orderId: string) {
+  console.log(`Getting the status of order ${orderId} `);
+  const orderAsNumber = parseInt(orderId);
+  if (orderAsNumber % 2 === 0) {
+    return "IN_PROGRESS";
+  }
+  return "COMPLETED";
+}
+
+async function callOpenAIWithTools() {
+  const context: OpenAI.Chat.ChatCompletionMessageParam[] = [
+    {
+      role: "developer",
+      content:
+        "You are a helpful assistant that gives info aabout the time of day and order status.",
+    },
+    {
+      role: "user",
+      content: "What is the status of ordewr 1902?",
+    },
+  ];
+
+  // configure chat tools (first openAI call)
+  const response = await openAI.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: context,
+    tools: [
+      {
+        type: "function",
+        function: {
+          name: "getTimeOfDay",
+          description: "Get the time of day",
+        },
+      },
+      {
+        type: "function",
+        function: {
+          name: "getOrderStatus",
+          description: "Returns the status of an order",
+          parameters: {
+            type: "object",
+            properties: {
+              orderId: {
+                type: "string",
+                description: "The id of the order to get the status of",
+              },
+            },
+            required: ["orderId"],
+          },
+        },
+      },
+    ],
+    tool_choice: "auto", // the engine will decide which tool to use
+  });
+
+  // decide if tool call is required based on the response
+  const willInvokeFunction: boolean =
+    response.choices[0].finish_reason === "tool_calls";
+  const toolCall = response.choices[0].message.tool_calls![0];
+
+  if (willInvokeFunction) {
+    const toolName = toolCall.function.name;
+
+    if (toolName === "getTimeOfDay") {
+      const toolResponse = getTimeOfDay();
+      context.push(response.choices[0].message);
+      context.push({
+        role: "tool",
+        content: toolResponse,
+        tool_call_id: toolCall.id,
+      });
+    }
+
+    if (toolName === "getOrderStatus") {
+      const rawArguments = toolCall.function.arguments;
+      const parsedArguments = JSON.parse(rawArguments);
+      const toolResponse = getOrderStatus(parsedArguments.orderId);
+      context.push(response.choices[0].message);
+      context.push({
+        role: "tool",
+        content: toolResponse,
+        tool_call_id: toolCall.id,
+      });
+    }
+  }
+
+  const secondResponse = await openAI.chat.completions.create({
+    model: "chatgpt-4o-mini",
+    messages: context,
+  });
+
+  console.log(secondResponse.choices[0].message.content);
+}
+
+callOpenAIWithTools();
+// invoke the tool
+// make a second openAI call with the tool response
